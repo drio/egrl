44483d1c421e87603cf14df807b69b9733f240e7
David Rio Deiros <driodeiros@gmail.com>
2013-02-18 16:22:07 -0600

    removing CS support

--- src/count.cpp
+++ src/count.cpp
@@ -35,7 +35,6 @@ static const char *COUNT_USAGE =
 "      -v, --verbose                    display verbose output.\n"
 "      -p, --probes=FILE                Probes file. Use - for standard input.\n"
 "      -r, --reads=FILE                 Reads file. Use - for standard input.\n"
-"      -c, --color_space                Input is in color space.\n"
 "      -t, --n_threads                  Number of threads [1].\n"
 "\nReport bugs to " PACKAGE_BUGREPORT "\n\n";
 
@@ -44,7 +43,6 @@ namespace opt
   static unsigned int verbose    = 0;
   static std::string reads_file  = "";
   static std::string probes_file = "";
-  static unsigned int cs_data    = 0; // Data in color space?
   static unsigned int n_threads  = 1;
 }
 
@@ -57,7 +55,6 @@ static const struct option longopts[] = {
   { "verbose"    ,         no_argument,       NULL, 'v' },
   { "probes"     ,         required_argument, NULL, 'p' },
   { "reads"      ,         required_argument, NULL, 'r' },
-  { "color_space",         no_argument,       NULL, 'c' },
   { "n_threads"  ,         no_argument,       NULL, 't' },
   { "help"       ,         no_argument,       NULL, OPT_HELP },
   { "version"    ,         no_argument,       NULL, OPT_VERSION },
@@ -71,7 +68,6 @@ void parse_count_options(int argc, char **argv)
     std::istringstream arg(optarg != NULL ? optarg : "");
     switch (c) {
       case 'v': opt::verbose++; break;
-      case 'c': opt::cs_data++; break;
       case 't': arg >> opt::n_threads; break;
       case 'p': arg >> opt::probes_file; break;
       case 'r': arg >> opt::reads_file; break;
@@ -105,7 +101,6 @@ void parse_count_options(int argc, char **argv)
     exit(EXIT_FAILURE);
   }
 }
-
 void load_probes(google::dense_hash_map<std::string, Probe*> &h_probes, std::istream *probes)
 {
   std::string line;
@@ -129,7 +124,7 @@ void load_probes(google::dense_hash_map<std::string, Probe*> &h_probes, std::ist
   }
 }
 
-void *screen_reads(google::dense_hash_map<std::string, Probe*> &h_probes,
+void *screen_reads(ss_probes *h_probes,
                    int probe_length,
                    std::string **buffer,
                    int n_reads,
@@ -149,7 +144,7 @@ void *screen_reads(google::dense_hash_map<std::string, Probe*> &h_probes,
       window        = buffer[p]->substr(i, probe_length);
       n_value[0]    = window[n_pos];
       window[n_pos] = 'N';
-      if ((p_iter = h_probes.find(window)) != h_probes.end()) {
+      if ((p_iter = h_probes->find(window)) != h_probes->end()) {
         pthread_mutex_lock(mutex);
         p_iter->second->update_counters(n_value);
         pthread_mutex_unlock(mutex);
@@ -165,124 +160,6 @@ void dump_results(google::dense_hash_map<std::string, Probe*> &h_probes)
   Probe *op; // Original probe
 
   google::dense_hash_map<std::string, Probe *>::iterator p_iter;
-  for (p_iter=h_probes.begin(); p_iter!=h_probes.end(); p_iter++) {
-    // In op we want always the original probe, not the rc one
-    op = p_iter->second->is_a_rc_probe ? p_iter->second->rc : p_iter->second;
-    if (!op->visited && (op->has_hits() || op->rc->has_hits())) {
-      op->get_counters(cs1);
-      op->rc->get_counters(cs2);
-      std::cout
-        << op->get_chrm() << ","
-        << op->get_coordinates() <<  ","
-        << op->get_id() <<  ","
-        << op->get_ref() <<  ","
-        << op->get_var() <<  ","
-
-        << cs1[0] << ","
-        << cs1[1] << ","
-        << cs1[2] << ","
-        << cs1[3] << ","
-        << cs1[4] << ","
-
-        << cs2[0] << ","
-        << cs2[1] << ","
-        << cs2[2] << ","
-        << cs2[3] << ","
-        << cs2[4] << ","
-
-        << cs1[0] + cs2[0] << ","
-        << cs1[1] + cs2[1] << ","
-        << cs1[2] + cs2[2] << ","
-        << cs1[3] + cs2[3] << ","
-        << cs1[4] + cs2[4]
-
-        << std::endl;
-
-      op->visited = 1;
-      op->rc->visited = 1;
-    }
-  }
-}
-
-void cs_load_probes(google::dense_hash_map<std::string, CSProbe*> &h_probes,
-                    std::istream *probes,
-                    CSUtils *csu)
-{
-  std::string line;
-  CSProbe *p, *rc_p;
-  Timer timer("Probe loading (CS)");
-  // TODO: Confirm that the probe size is constant
-  //
-  // TODO: There is no need to store all the sequence information of the probe
-  while (getline(*probes, line)) {
-    // TODO: check if probe already there, if so, ignore, otherwise we will have memory leaks
-    p       = new CSProbe(line, csu);
-    rc_p    = new CSProbe(line, csu);
-    rc_p->set_rc();
-
-    rc_p->set_rc_p(p);
-    p->set_rc_p(rc_p);
-
-    std::string key = p->get_cs_five_p() + "NN" + p->get_cs_three_p();
-    h_probes[key]   = p;
-    key             = rc_p->get_cs_five_p() + "NN" + rc_p->get_cs_three_p();
-    h_probes[key]   = rc_p;
-  }
-}
-
-void *cs_screen_reads(google::dense_hash_map<std::string, CSProbe*> &h_probes,
-                     int probe_length,
-                     std::string **buffer,
-                     int n_reads,
-                     int tid,
-                     pthread_mutex_t *mutex,
-                     int n_threads)
-{
-  google::dense_hash_map<std::string, CSProbe *>::iterator p_iter;
-  const int n_pos = (probe_length/2)-1;
-  std::string window;
-  std::string n_value("  ");
-
-  int p,i; // pointer to buffer, pointer in read
-	for (p=0; p!=n_reads; ++p) {
-    if ((n_threads>1) && (p % n_threads != tid)) continue;
-    for (i=0; i+probe_length <= (int)buffer[p]->length(); ++i) {
-      window          = buffer[p]->substr(i,probe_length);
-      n_value[0]      = window[n_pos];
-      n_value[1]      = window[n_pos+1];
-      window[n_pos]   = 'N';
-      window[n_pos+1] = 'N';
-      if ((p_iter = h_probes.find(window)) != h_probes.end()) {
-        pthread_mutex_lock(mutex);
-        p_iter->second->update_counters(n_value);
-        pthread_mutex_unlock(mutex);
-      }
-    }
-  }
-  return 0;
-
-  /*
-  while(reader.get(record)) {
-    read = record.seq.toString();
-    for (unsigned int i=0; i+probe_length<=read.length(); i++) {
-      window          = read.substr(i,probe_length);
-      n_value[0]      = window[n_pos];
-      n_value[1]      = window[n_pos+1];
-      window[n_pos]   = 'N';
-      window[n_pos+1] = 'N';
-      if ((p_iter = h_probes.find(window)) != h_probes.end())
-        p_iter->second->update_counters(n_value);
-    }
-  }
-  */
-}
-
-void cs_dump_results(google::dense_hash_map<std::string, CSProbe*> &h_probes)
-{
-  int cs1[5], cs2[5];
-  CSProbe *op; // Original probe
-
-  google::dense_hash_map<std::string, CSProbe *>::iterator p_iter;
   for (p_iter=h_probes.begin(); p_iter!=h_probes.end(); p_iter++) {
     // In op we want always the original probe, not the rc one
     op = p_iter->second->is_a_rc_probe ? p_iter->second->rc : p_iter->second;
@@ -340,12 +217,8 @@ int load_to_buffer(SeqReader *reader, std::string **buffer)
 static void *worker(void *data)
 {
 	thread_data *d = (thread_data*) data;
-  if (d->cs_data)
-    cs_screen_reads(d->probes->cs, d->p_len, d->buffer,
-                    d->n_reads, d->tid, d->mutex, d->n_threads);
-  else
-	  screen_reads(d->probes->ss, d->p_len, d->buffer,
-                 d->n_reads, d->tid, d->mutex, d->n_threads);
+  screen_reads(d->probes, d->p_len, d->buffer,
+               d->n_reads, d->tid, d->mutex, d->n_threads);
 	return 0;
 }
 
@@ -359,37 +232,25 @@ void count_main(int argc, char **argv)
                   &std::cin : createReader(opt::probes_file);
 
   /* Load probes */
-  CSUtils csu;
-  ss_or_cs_probes probes;
+  ss_probes probes;
   int probe_length;
   std::cerr << ">> Loading probes " << std::endl;
-  if (opt::cs_data) {
-    probes.cs.set_empty_key("-");
-    cs_load_probes(probes.cs, probes_stream, &csu);
-    std::cerr << ">> # of CS probes (RC included): " << probes.cs.size() << std::endl;
-    probe_length = probes.cs.begin()->first.length();
-  }
-  else {
-    probes.ss.set_empty_key("-");
-    load_probes(probes.ss, probes_stream);
-    std::cerr << ">> # of probes (RC included): " << probes.ss.size() << std::endl;
-    probe_length = probes.ss.begin()->first.length();
-  }
+  probes.set_empty_key("-");
+  load_probes(probes, probes_stream);
+  std::cerr << ">> # of probes (RC included): " << probes.size() << std::endl;
+  probe_length = probes.begin()->first.length();
 
   /* Process reads */
   std::string **buffer;
   // request mem for buffer of reads
   buffer = (std::string **) calloc(sizeof(std::string *), BUFFER_SIZE);
   int n_rr  = 0;  // number of reads read
-  int total = 0; // total number of reads processed
+  int total = 0;  // total number of reads processed
   SeqReader reader(opt::reads_file, SRF_NO_VALIDATION);
   while((n_rr = load_to_buffer(&reader, buffer)) != 0) {
     Timer *pr_timer = new Timer("Processing reads");
     std::cerr << ">> Computing screen on " << n_rr << " reads" << std::endl;
     total += n_rr;
-    // Do stuff with data in buffer
-    //if (opt::cs_data) count_in_cs(probes_stream);
-    //else              count_in_ss(probes_stream);
     pthread_t *tid;
     pthread_attr_t attr;
     thread_data *data;
@@ -407,7 +268,6 @@ void count_main(int argc, char **argv)
       data[j].buffer    = buffer;
       data[j].probes    = &probes;
       data[j].p_len     = probe_length;
-      data[j].cs_data   = opt::cs_data;
       data[j].mutex     = &mutex;
       data[j].n_threads = opt::n_threads;
       pthread_create(&tid[j], &attr, worker, data+j);
@@ -420,9 +280,7 @@ void count_main(int argc, char **argv)
     delete pr_timer; // force dump cpu time.
   }
 
-  // Dump results
-  if (opt::cs_data) cs_dump_results(probes.cs);
-  else dump_results(probes.ss);
+  dump_results(probes);
 
   delete probes_stream;
   for (int j=0; j<BUFFER_SIZE; j++) delete buffer[j];
--- src/count.h
+++ src/count.h
@@ -2,43 +2,31 @@
 #define COUNT_H
 
 #include "probe.h"
-#include "cs_probe.h"
-#include "cs_utils.h"
 #include "Util/Util.h"
 #include "Util/SeqReader.h"
 
 #include <google/sparse_hash_map>
 #include <google/dense_hash_map>
 
-typedef struct {
-  google::dense_hash_map<std::string, Probe*> ss;
-  google::dense_hash_map<std::string, CSProbe*> cs;
-} ss_or_cs_probes;
+typedef google::dense_hash_map<std::string, Probe*> ss_probes;
 
 typedef struct {
   int  n_reads;
   int  tid;
   std::string **buffer;
-  ss_or_cs_probes *probes;
+  ss_probes *probes;
   int p_len;
-  int cs_data;
   pthread_mutex_t *mutex;
   int n_threads;
 } thread_data;
 
 void parse_count_options(int, char **);
-void count_main(int, char **);
 void load_probes(google::dense_hash_map<std::string, Probe*> *, std::istream *);
-void *screen_reads(google::dense_hash_map<std::string, Probe*> *, int);
+void *screen_reads(ss_probes *, int, std::string **, int, int, pthread_mutex_t *, int);
+
 void dump_results(google::dense_hash_map<std::string, Probe*> &);
-
-void cs_load_probes(google::dense_hash_map<std::string, CSProbe*> *, std::istream *, CSUtils *);
-void *cs_screen_reads(google::dense_hash_map<std::string, CSProbe*> *, int);
-void cs_dump_results(google::dense_hash_map<std::string, CSProbe*> &);
-
-void count_in_ss(std::istream*);
-void count_in_cs(std::istream*);
-
 int load_to_buffer(SeqReader *, std::string **);
+
+void count_main(int, char **);
 
 #endif
